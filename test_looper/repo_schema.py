6f5612109194f91380e72f1e14dfb1b980096d46
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-07-30 20:33:59 -0700

    Add functionality to create, list, and get repositories
    Now you can do something like:
    ```
    service = LooperService.from_odb(db)
    service.add_repo('test-looper', 'https://github.com/aprioriinvestments/test-looper')
    config = service.get_repo_config('test-looper')
    
    for name, config in service.get_all_repos().items():
        pass
    ```

---
+++ test_looper/repo_schema.py
@@ -0,0 +1,53 @@
+# Define the schema related to repos, branchs, and commits
+from typed_python import Alternative
+from object_database import Indexed
+
+
+from test_looper import test_looper_schema
+
+
+# describe generic services, which can provide lots of different repos
+GitService = Alternative(
+    "GitService",
+    Github=dict(
+        oauth_key=str,
+        oauth_secret=str,
+        webhook_secret=str,
+        owner=str,  # owner we use to specify which projects to look at
+        access_token=str,
+        auth_disabled=bool,
+        github_url=str,  # usually https://github.com
+        github_login_url=str,  # usually https://github.com
+        github_api_url=str,  # usually https://github.com/api/v3
+        github_clone_url=str,  # usually git@github.com
+    ),
+    Gitlab=dict(
+        oauth_key=str,
+        oauth_secret=str,
+        webhook_secret=str,
+        group=str,  # group we use to specify which projects to show
+        private_token=str,
+        auth_disabled=bool,
+        gitlab_url=str,  # usually https://gitlab.mycompany.com
+        gitlab_login_url=str,  # usually https://gitlab.mycompany.com
+        gitlab_api_url=str,  # usually https://gitlab.mycompany.com/api/v3
+        gitlab_clone_url=str,  # usually git@gitlab.mycompany.com
+    ),
+)
+
+
+# describe how to get access to a specific repo
+RepoConfig = Alternative(
+    "RepoConfig",
+    Ssh=dict(url=str, private_key=bytes),
+    Https=dict(url=str),
+    Local=dict(path=str),
+    S3=dict(url=str),
+    FromService=dict(repo_name=str, service=GitService),
+)
+
+
+@test_looper_schema.define
+class Repository:
+    config = RepoConfig
+    name = Indexed(str)
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-import pytest
+from urllib.parse import urlparse
 
 from object_database.database_connection import DatabaseConnection
+import pytest
 
-from test_looper.service import LooperService
+from test_looper.service import LooperService, parse_repo_url
 from test_looper.service_schema import Config, ArtifactStorageConfig
+from test_looper.repo_schema import Repository, RepoConfig
 from conftest import odb_conn
 
 
@@ -38,3 +40,49 @@ def test_create_service(odb_conn: DatabaseConnection, tl_config):
     assert service.repo_url == tl_config["repo_url"]
     assert service.temp_url == tl_config["temp_url"]
     assert service.artifact_store == tl_config["artifact_store"]
+
+
+def test_add_repo(odb_conn: DatabaseConnection, tl_config):
+    service = LooperService.from_odb(odb_conn)
+    # TODO test the FromService variant
+    # test_parse_repo tests adding the other types
+    repos = {
+        "test-looper": "https://github.com/aprioriinvestments/test-looper",
+        "typed_python": "https://github.com/aprioriinvestments/typed_python",
+        "odb": "https://github.com/aprioriinvestments/object_database",
+    }
+    for name, url in repos.items():
+        with service.add_repo(name, url) as repo:
+            assert repo.name == name
+            assert repo.config.url == url
+
+    all_repos = service.get_all_repos()
+    for name, config in all_repos.items():
+        assert repos[name] == config.url
+
+    for name, url in repos.items():
+        config = service.get_repo_config(name)
+        assert isinstance(config, RepoConfig.Https)
+        assert config.url == url
+
+
+def test_parse_repo():
+    alternatives = [
+        (RepoConfig.Ssh, "ssh://user@foo.com:org/repo", {"private_key": b""}),
+        (RepoConfig.Ssh, "git@github.com:org/repo", {"private_key": b""}),
+        (RepoConfig.Https, "https://github.com/org/repo", {}),
+        (RepoConfig.Local, "file:///path/to/repo", {}),
+        (RepoConfig.Local, "/path/to/repo", {}),
+        (RepoConfig.S3, "s3://bucket/path/to/repo", {}),
+    ]
+    for i, (klass, conf_str, kwargs) in enumerate(alternatives):
+        _check_str_repo_config(klass, f"tl-{i}", conf_str, **kwargs)
+
+
+def _check_str_repo_config(klass, name, url, **kwargs):
+    config = parse_repo_url(url, **kwargs)
+    assert isinstance(config, klass)
+    if klass == RepoConfig.Local:
+        assert config.path == urlparse(url).path
+    else:
+        assert config.url == url
