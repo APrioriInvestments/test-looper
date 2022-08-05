8a65480cd2ac4ec92cbb521a091ff0e885a2d6b8
dkrasner <krasnerdaniel@gmail.com>
2022-08-05 19:35:25 +0000

    adding more panels and better resizing

--- test_looper/cells.py
+++ test_looper/cells.py
@@ -33,10 +33,16 @@ class TLService(ServiceBase):
 
         return cells.VCenter(
             cells.ResizablePanel(
-                cells.FillSpace(selections_card(), horizontal="center", vertical="top"),
+                cells.FillSpace(
+                    selections_card(), horizontal="center", vertical="top"),
                 cells.ResizablePanel(
-                    cells.FillSpace(test_results_table(), horizontal="center", vertical="top"),
-                    cells.FillSpace(plots_card(), horizontal="center", vertical="top"),
+                    cells.FillSpace(
+                        test_results_table(), horizontal="center",
+                        vertical="top"
+                    ),
+                    cells.FillSpace(
+                        plots_card(), horizontal="center", vertical="top"
+                    ),
                 )
             )
         )
@@ -44,23 +50,24 @@ class TLService(ServiceBase):
 
 # helper functions
 
-### Reporting ###
+# Reporting ###
 # I display test run resports #
 def test_results_table():
     return cells.Card(
         cells.Highlighted(
-        cells.Table(
-            colFun=lambda: ['name', 'testsDefined', 'needsMoreWork'],
-            rowFun=lambda: TestNode.lookupAll(),
-            headerFun=lambda x: x,
-            rendererFun=test_results_table_render_fun(),
-            maxRowsPerPage=100,
-            fillHeight=True
-            ), color="lightblue"
+            cells.Table(
+                colFun=lambda: ['name', 'testsDefined', 'needsMoreWork'],
+                rowFun=lambda: TestNode.lookupAll(),
+                headerFun=lambda x: x,
+                rendererFun=test_results_table_render_fun(),
+                maxRowsPerPage=100,
+                fillHeight=True
+                ), color="lightblue"
         ),
         header="Test reporting",
         padding=5
     )
+
 
 def test_results_table_render_fun():
     return lambda n, col: cells.Subscribed(
@@ -69,7 +76,8 @@ def test_results_table_render_fun():
             n.testsDefined if col == 'testsDefined'
         else n.needsMoreWork)
 
-### Plots & Graphs ###
+
+# Plots & Graphs ###
 def plots_card():
     return cells.Card(
         cells.Highlighted(
@@ -80,29 +88,67 @@ def plots_card():
         padding=5
     )
 
-### Selections ###
+
+# Selections ###
 # Branches and Dropdowns
 def selections_card():
+    repo_slot = cells.Slot("dev")
     branch_slot = cells.Slot("dev")
     commit_slot = cells.Slot("last commit")
-    return cells.Card(
-        cells.Highlighted(
-            cells.Subscribed(
-                lambda: cells.Dropdown(
-                    "Git branch: " + str(branch_slot.get()),
-                    ["branch1", "branch2", "branch3"],
-                    lambda i: branch_slot.set(i)
-                )
+    default_repo_url = "https://github.com/APrioriInvestments/test-looper"
+    return cells.Highlighted(
+        cells.Card(
+            cells.SingleLineTextBox(
+                default_repo_url,
+                onEnter=lambda text: repo_slot.set(text)
             ) +
-            cells.Subscribed(
-                lambda: cells.Dropdown(
-                    "Git commit: " + str(commit_slot.get()),
-                    ["commit1", "commit2", "commit3"],
-                    lambda i: commit_slot.set(i)
-                )
-            ), color="lightblue"
-        ),
-        header="Branch and commit selection",
-        padding=5
+            cells.HorizontalSequence(
+                [
+                    cells.Card(
+                        cells.FillSpace(
+                            cells.Subscribed(
+                                lambda: cells.Dropdown(
+                                    "Git branch: " + str(branch_slot.get()),
+                                    ["branch1", "branch2", "branch3"],
+                                    lambda i: branch_slot.set(i)
+                                )
+                            ) +
+                            cells.FillSpace(
+                                cells.Subscribed(
+                                    lambda: cells.Dropdown(
+                                        "Git commit: " + str(commit_slot.get()
+                                                             ),
+                                        ["commit1", "commit2", "commit3"],
+                                        lambda i: commit_slot.set(i)
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    cells.Card(
+                        cells.FillSpace(
+                            info_panel()
+                        )
+                    ),
+                ]
+            ),
+            header="Branch and commit selection",
+            padding=5
+        ), color="lightblue"
     )
 
+
+def info_panel():
+    is_open = cells.Slot(False)
+
+    return cells.Subscribed(
+        lambda: cells.Button(
+            "Close" if is_open.get() else "More info",
+            lambda: is_open.set(not is_open.get()),
+        ) +
+        cells.CollapsiblePanel(
+            panel=cells.Text("more info about the repo"),
+            content=cells.Text(""),
+            isExpanded=lambda: is_open.get()
+        )
+    )
