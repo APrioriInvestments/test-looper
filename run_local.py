b3a0f5f3c3294a1eb988e736b423a0e1c074b682
William Grant <william.grant@aprioriinvestments.com>
2023-03-28 15:41:44 -0400

    initial commit, following cells demo

---
+++ run_local.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+#   Copyright 2023 Braxton Mckee
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import sys
+import tempfile
+import time
+
+from object_database.service_manager.ServiceManager import ServiceManager
+
+from testlooper.service import TestlooperService
+
+from object_database.web.ActiveWebServiceSchema import active_webservice_schema
+from object_database.web.ActiveWebService import ActiveWebService
+from object_database import connect, core_schema, service_schema
+from object_database.frontends.service_manager import startServiceManagerProcess
+from object_database.util import genToken
+from object_database.web.LoginPlugin import LoginIpPlugin
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    token = genToken()
+    httpPort = 8000
+    odbPort = 8020
+    loglevel_name = "INFO"
+
+    with tempfile.TemporaryDirectory() as tmpDirName:
+        server = None
+        try:
+            server = startServiceManagerProcess(
+                tmpDirName, odbPort, token, loglevelName=loglevel_name, logDir=False
+            )
+
+            database = connect("localhost", odbPort, token, retry=True)
+            database.subscribeToSchema(core_schema, service_schema, active_webservice_schema)
+
+            with database.transaction():
+                service = ServiceManager.createOrUpdateService(
+                    ActiveWebService, "ActiveWebService", target_count=0
+                )
+
+            ActiveWebService.configureFromCommandline(
+                database,
+                service,
+                [
+                    "--port",
+                    str(httpPort),
+                    "--internal-port",
+                    "8001",
+                    "--host",
+                    "0.0.0.0",
+                    "--log-level",
+                    loglevel_name,
+                ],
+            )
+
+            ActiveWebService.setLoginPlugin(
+                database,
+                service,
+                LoginIpPlugin,
+                [None],
+                config={"company_name": "A Testing Company"},
+            )
+
+            with database.transaction():
+                ServiceManager.startService("ActiveWebService", 1)
+
+            with database.transaction():
+                service = ServiceManager.createOrUpdateService(
+                    TestlooperService, "TestlooperService", target_count=1
+                )
+
+            while True:
+                time.sleep(0.1)
+        finally:
+            if server is not None:
+                server.terminate()
+                server.wait()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- testlooper/schema.py
+++ testlooper/schema.py
@@ -1,10 +1,11 @@
 """Define the core objects and ODB schema for test-looper."""
-from typed_python import OneOf, Alternative, TupleOf, Dict
+from typed_python import OneOf, Alternative, TupleOf, Dict, NamedTuple
 from object_database import Schema, Indexed, Index, SubscribeLazilyByDefault
 
+__all__ = ["test_looper_schema"]
 
 # primary schema for test-looper objects
-test_looper_schema = Schema("test_looper")
+test_looper_schema = Schema("testlooper")
 
 
 # describe generic services, which can provide lots of different repos
@@ -50,7 +51,6 @@ RepoConfig = Alternative(
 )
 
 
-
 ArtifactStorageConfig = Alternative(
     "ArtifactStorageConfig",
     LocalDisk=dict(
@@ -78,13 +78,13 @@ Command = NamedTuple(
 
 
 # model a single entry in a test plan. The collection of these must form a DAG
-# with 'Test' 
+# with 'Test'
 TestNodeDefinition = Alternative(
     "TestNodeDefinition",
     # run arbitrary code to produce a set of binary outputs
     Build=dict(
-        inputs=TupleOf(str), # the named inputs
-        outputs=TupleOf(str), # list of named outputs
+        inputs=TupleOf(str),  # the named inputs
+        outputs=TupleOf(str),  # list of named outputs
         minCpus=int,
         minRamGb=float,
         command=Command,
@@ -92,11 +92,11 @@ TestNodeDefinition = Alternative(
     # run arbitrary code to produce the text of a docker image
     # then build that docker image
     DockerImage=dict(
-        inputs=TupleOf(str), # the named inputs
+        inputs=TupleOf(str),  # the named inputs
     ),
     # run arbitrary code to produce a list of tests, and then run them
     Test=dict(
-        inputs=TupleOf(str), # named inputs
+        inputs=TupleOf(str),  # named inputs
         minCpus=int,
         minRamGb=float,
         listTests=Command,
@@ -131,7 +131,8 @@ WorkerConfig = Alternative(
         worker_name=str,  # name of workers. This should be unique to this install.
         worker_iam_role_name=str,  # AIM role to boot workers into
         linux_ami=str,  # default linux AMI to use when booting linux workers
-        windows_ami=str,  # default AMI to use when booting windows workers. Can be overridden for one-shot workers.
+        # default AMI to use when booting windows workers. Can be overridden for one-shot workers.
+        windows_ami=str,
         path_to_keys=str,  # path to ssh keys to place on workers to access source control.
         instance_types=Dict(HardwareConfig, str),
         # dict from hardware configuration to instance types we're willing to boot
@@ -139,23 +140,32 @@ WorkerConfig = Alternative(
         # dict from hostname to ip address to make available to workers
         # this is primarily useful when workers don't have access to dns
         # but we still want certs to all be valid
-        max_cores=OneOf(None, int),  # cap on the number of cores we're willing to boot. None means no limit
-        max_ram_gb=OneOf(None, int),  # cap on the number of gb of ram we're willing to boot. None means no limit
-        max_workers=OneOf(None, int),  # cap on the number of workers we're willing to boot. None means no limit
+        # cap on the number of cores we're willing to boot. None means no limit
+        max_cores=OneOf(None, int),
+        # cap on the number of gb of ram we're willing to boot. None means no limit
+        max_ram_gb=OneOf(None, int),
+        # cap on the number of workers we're willing to boot. None means no limit
+        max_workers=OneOf(None, int),
     ),
     # run workers in-proc in the server
     Local=dict(
         local_storage_path=str,  # local disk storage we can use for workers
         docker_scope=str,  # local scope to augment dockers with
-        max_cores=OneOf(None, int),  # cap on the number of cores we're willing to boot. means no limit
-        max_ram_gb=OneOf(None, int),  # cap on the number of gb of ram we're willing to boot. means no limit
-        max_workers=OneOf(None, int),  # cap on the number of workers we're willing to boot. means no limit
+        # cap on the number of cores we're willing to boot. means no limit
+        max_cores=OneOf(None, int),
+        # cap on the number of gb of ram we're willing to boot. means no limit
+        max_ram_gb=OneOf(None, int),
+        # cap on the number of workers we're willing to boot. means no limit
+        max_workers=OneOf(None, int),
     ),
     # run workers in-proc in the server
     Dummy=dict(
-        max_cores=OneOf(None, int),  # cap on the number of cores we're willing to boot. None means no limit
-        max_ram_gb=OneOf(None, int),  # cap on the number of gb of ram we're willing to boot. None means no limit
-        max_workers=OneOf(None, int),  # cap on the number of workers we're willing to boot. None means no limit
+        # cap on the number of cores we're willing to boot. None means no limit
+        max_cores=OneOf(None, int),
+        # cap on the number of gb of ram we're willing to boot. None means no limit
+        max_ram_gb=OneOf(None, int),
+        # cap on the number of workers we're willing to boot. None means no limit
+        max_workers=OneOf(None, int),
     )
 )
 
@@ -232,15 +242,6 @@ class Branch:
 
 
 @test_looper_schema.define
-class Branch:
-    repo = Indexed(Repo)
-    name = str
-
-    repoAndName = Index('repo', 'name')
-    topCommit = Indexed(Commit)
-
-
-@test_looper_schema.define
 class TestNode:
     commit = Indexed(Commit)
     name = str
@@ -267,6 +268,7 @@ class TestNode:
 
     # crude proxy for the state machine we'll need to decide what to build/run
     needsMoreWork = Indexed(bool)
+
 
 @test_looper_schema.define
 @SubscribeLazilyByDefault
